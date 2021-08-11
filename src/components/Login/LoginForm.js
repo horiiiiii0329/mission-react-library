1146e7309fd4fe281597680c07c63a78979c0296
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-08-11 19:32:29 +0900

    stage1

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,12 +1,65 @@
 import classes from "./LoginForm.module.css";
+import { useState, useRef } from "react";
 
 const LoginForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredpassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+    fetch(
+      "https://app.swaggerhub.com/apis-docs/Takumaron/TechTrain-RailwayMission/1.0.0#/user/post_signin",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredpassword,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = data.ErrorMessageJP;
+          setErrorMessage(errorMessage);
+          throw new Error(errorMessage);
+        });
+      }
+    });
+  };
+
   return (
-    <form>
-      <div classname={classes.control}>
-        <label htmlFor="name"></label>
-      </div>
-    </form>
+    <section className={classes.auth}>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">e-mail</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">パスワード</label>
+          <input type="password" id="password" ref={passwordInputRef} />
+        </div>
+        {isLoading && <p>{errorMessage}</p>}
+        <div className={classes.actions}>
+          <button type="button" className={classes.toggle}>
+            ログイン
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -1,21 +1,65 @@
 import classes from "./SignupForm.module.css";
+import { useState, useRef } from "react";
 
 const SignupForm = () => {
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredpassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+    fetch(
+      "https://app.swaggerhub.com/apis-docs/Takumaron/TechTrain-RailwayMission/1.0.0#/user/post_users",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          name: enteredName,
+          email: enteredEmail,
+          password: enteredpassword,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = data.ErrorMessageJP;
+          setErrorMessage(errorMessage);
+          throw new Error(errorMessage);
+        });
+      }
+    });
+  };
+
   return (
     <section className={classes.auth}>
-      <form>
-        <div classname={classes.control}>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
           <label htmlFor="name">名前</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" ref={nameInputRef} />
         </div>
-        <div classname={classes.control}>
+        <div className={classes.control}>
           <label htmlFor="email">e-mail</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
-        <div classname={classes.control}>
+        <div className={classes.control}>
           <label htmlFor="password">パスワード</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" ref={passwordInputRef} />
         </div>
+        {isLoading && <p>{errorMessage}</p>}
         <div className={classes.actions}>
           <button type="button" className={classes.toggle}>
             サインアップ
