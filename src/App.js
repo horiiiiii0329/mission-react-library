da6a121225de10f1dc4c418f94b9c7757cd85416
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-08-11 17:14:28 +0900

    ルーティングの作成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
-import { Switch, Router } from "react-router";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/signup">
+          <SignupPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <h1>LogIn</h1>;
+};
+
+export default LoginPage;
