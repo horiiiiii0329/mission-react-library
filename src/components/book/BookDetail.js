f72f9da32117893209187fb3f1dcd74fe32de2bd
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-08-14 12:40:21 +0900

    step7

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,45 +1,47 @@
 import classes from "./BookDetail.module.css";
 import { useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 const BookDetail = () => {
+  const authCtx = useContext(AuthContext);
   const { id } = useParams();
+  const [bookDetail, setBookdetail] = useState(null);
 
-  //   async function getSingleQuote(quoteId) {
-  //     const response = await fetch(`${FIREBASE_DOMAIN}/quotes/${quoteId}.json`);
-  //     const data = await response.json();
-
-  //     if (!response.ok) {
-  //       throw new Error(data.message || "Could not fetch quote.");
-  //     }
-
-  //     const loadedQuote = {
-  //       id: quoteId,
-  //       ...data,
-  //     };
-
-  //     return loadedQuote;
-  //   }
+  useEffect(() => {
+    fetch(`https://api-for-missions-and-railways.herokuapp.com/books/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${authCtx.token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setBookdetail(data))
+      .catch((err) => alert(err));
+  });
 
   return (
-    <div className={classes.detail}>
-      <div className={classes.title}>
-        <h3>タイトル</h3>
-        <p>LOLLOLOLOL</p>
-        <a>wwwwwwwwwwwwwwwwwwwwwwww</a>
-        <hr></hr>
-      </div>
-      <div>
-        <h3>詳細</h3>
-        <p>
-          wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-        </p>
-      </div>
-      <div className={classes.comments}>
-        <h3>レビュー</h3>
-        <span>ユーザー名</span>
-        <p>めっちゃよかったです。</p>
-      </div>
-    </div>
+    <>
+      {bookDetail && (
+        <div className={classes.detail}>
+          <div className={classes.title}>
+            <h3>タイトル</h3>
+            <p>{bookDetail.title}</p>
+            <a href={bookDetail.url}>{bookDetail.url.substring(0, 40)}...</a>
+            <hr></hr>
+          </div>
+          <div>
+            <h3>詳細</h3>
+            <p>{bookDetail.detail}</p>
+          </div>
+          <div className={classes.comments}>
+            <h3>レビュー</h3>
+            <span>{bookDetail.reviewer}</span>
+            <p>{bookDetail.review}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
